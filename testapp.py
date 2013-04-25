19baaf8872199afa802e105682c7e8c7193c2bd7
Katrina Magalotti <kmagalotti.student@manhattan.edu>
2013-04-24 23:53:22 -0400

    too tired to make it work

--- testapp.py
+++ testapp.py
@@ -1,4 +1,4 @@
-import cgi
+import cgi, cgitb
 import webapp2
 
 from google.appengine.api import users
@@ -11,28 +11,53 @@ class MainPage(webapp2.RequestHandler):
               <title> Knitting Page </title>
             </head>
             <body>
-              <form action="/sign" method="post">
-                <div> Stitches per square inch (height): <input type="integer" name="height"/></div>
-                <div> Stitches per square inch (width): <input type="integer" name="width"/></div>
-                <div> Hips: <input type="integer" name="hips"/></div>
-                <div> Waist: <input type="integer" name="waist"/></div>
-                <div> Bust: <input type="integer" name="bust"/></div>
-                <div> Shoulder Width: <input type="integer" name="shoulder"/></div>
-                <div> Length: <input type="integer" name="length"/></div>
-                <div><input type="submit" value="Calculate"></div>
+              <form action="/print" method="get">
+                Gauge Square:
+                Stitches per square inch (height): <input type="integer" name="height"/> <br />
+                Stitches per square inch (width): <input type="integer" name="width"/> <br />
+                Length of yarn (yards): <input type="integer" name="gauge"/> <br />
+                <br />
+                Size: <br />
+                <input type="radio" name="size" value="xs" /> X-Small <br />
+                <input type="radio" name="size" value="s" /> Small <br />
+                <input type="radio" name="size" value="m" /> Medium <br />
+                <input type="radio" name="size" value="l" /> Large <br />
+                <input type="radio" name="size" value="xl" /> X-Large <br />
+                <input type="submit" value="Calculate">
               </form>
             </body>
           </html>""")
 
-
-class Guestbook(webapp2.RequestHandler):
-    def post(self):
-        leng = cgi.FieldStorage()
-        self.response.out.write(leng["length"])
+class Sweater(webapp2.RequestHandler):
+    def get(self):
+        bust = [29, 33, 37, 41, 45]
+        neckCuff = [27.5, 28.5, 29.5, 30.5, 31.5] #center back to neck cuff
+        length2 = [16.5, 17, 17.25, 17.5, 17.75] #back waist length
+        shoulder = [14.5, 15, 16.5, 17.5, 17.5] #cross back
+        sleeve = [16.5, 17, 17, 17.5, 17.5] #sleeve length to underarm
+        arm = [9.75, 10.25, 11, 12, 13.5]
+        armhole = [6.5, 7, 7.5, 8, 8.5] #depth
+        waist = [23.5, 26, 29, 31, 37]
+        hips = [33.5, 35.5, 39, 43, 47]
+        form = cgi.FieldStorage()
+        strSize = form.getvalue('size')
+        if strSize == 'xs':
+            size = 0
+        elif strSize == 's':
+            size = 1
+        elif strSize == 'm':
+            size = 2
+        elif strSize == 'l':
+            size = 3
+        else strSize == 'xl':
+            size = 4
+        #data_uri = open('standard.png', 'rb').read().encode('base64').replace('\n', '')
+        #img_tag = '<img src="data:image/png;base64,{0}">'.format(data_uri)
+        #self.response.out.write(img_tag)
         self.response.out.write('<html><body>You wrote:<pre>')
-        self.response.out.write(cgi.escape(self.request.get('length')))
+        self.response.out.write(str(size))
         self.response.out.write('</pre></body></html>')
 
 app = webapp2.WSGIApplication([('/', MainPage),
-                              ('/sign', Guestbook)],
+                              ('/print', Sweater)],
                               debug=True)
